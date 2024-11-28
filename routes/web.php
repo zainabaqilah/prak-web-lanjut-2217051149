1d96290f93c3b5e92fc764ff8be5417802244391
zainabaqilah <zainabb31864@gmail.com>
2024-11-28 15:43:07 +0700

    create:storage-file

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/user/create', [UserController::class, 'create'])->name('user.create
 
 Route::post('/user/store', [UserController::class, 'store'])->name('user.store'); 
 
-Route::get('/user', [UserController::class, 'index']);
+Route::get('/user', [UserController::class, 'index'])->name('user.list');
 
 Route::get('/show/{id}', [UserController::class, 'show'])->name('user.show');
 
